68f5bb49b92a257f1f89cc6fb29a80892158217e
root <root@waveshine01.lan>
2018-10-24 05:21:08 +0000

    update for pi flashing

--- piflash/piflash.sh
+++ piflash/piflash.sh
@@ -9,12 +9,13 @@ echo out > /sys/class/gpio/gpio3/direction || true
 
 echo "Continuing"
 
-echo 1 > /sys/class/gpio/gpio2/value # Power
+echo 0 > /sys/class/gpio/gpio2/value # Power
 echo 0 > /sys/class/gpio/gpio3/value # GPIO0
 
-sleep .3
+sleep .5
 # Boot up in bootloader mode.
-echo 0 > /sys/class/gpio/gpio2/value
+echo 1 > /sys/class/gpio/gpio2/value
+sleep .5
 
 #killall hciattach || true
 #stty -F /dev/ttyAMA0 115200 -echo raw
@@ -22,11 +23,12 @@ echo 0 > /sys/class/gpio/gpio2/value
 esptool/esptool.py  -b 1000000 --port /dev/serial0 write_flash  0x00000 image.elf-0x00000.bin 0x10000 image.elf-0x10000.bin  532480 page.mpfs
 
 
-echo 1 > /sys/class/gpio/gpio2/value # Power
+echo 0 > /sys/class/gpio/gpio2/value # Power
 echo 1 > /sys/class/gpio/gpio3/value # GPIO0
 sleep .3
-echo 0 > /sys/class/gpio/gpio2/value
+echo 1 > /sys/class/gpio/gpio2/value
 
 # -fm dio 
 # 532480  web/page.mpfs
+
 
