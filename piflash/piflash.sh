8c714931b59ec1019199d78759e6336a86d33a10
cnlohr <lohr85@gmail.com>
2018-10-24 17:44:54 +0000

    update code - works to flash thing

--- piflash/piflash.sh
+++ piflash/piflash.sh
@@ -1,16 +1,32 @@
 #!/bin/sh -e
 
-true || echo 2 > /sys/class/gpio/export
-true || echo 3 > /sys/class/gpio/export
-true || echo out > /sys/class/gpio/gpio2/direction
-true || echo out > /sys/class/gpio/gpio3/direction
+echo GENERAL NOTE: have you made sure to use uart_tool to switch mode to gpio?
+
+echo 2 > /sys/class/gpio/export || true
+echo 3 > /sys/class/gpio/export || true
+echo out > /sys/class/gpio/gpio2/direction || true
+echo out > /sys/class/gpio/gpio3/direction || true
 
 echo "Continuing"
 
 echo 1 > /sys/class/gpio/gpio2/value # Power
 echo 0 > /sys/class/gpio/gpio3/value # GPIO0
 
-sleep .2
+sleep .3
 # Boot up in bootloader mode.
 echo 0 > /sys/class/gpio/gpio2/value
+
+#killall hciattach || true
+#stty -F /dev/ttyAMA0 115200 -echo raw
+
+esptool/esptool.py  -b 1000000 --port /dev/serial0 write_flash  0x00000 image.elf-0x00000.bin 0x10000 image.elf-0x10000.bin  532480 page.mpfs
+
+
+echo 1 > /sys/class/gpio/gpio2/value # Power
+echo 1 > /sys/class/gpio/gpio3/value # GPIO0
+sleep .3
+echo 0 > /sys/class/gpio/gpio2/value
+
+# -fm dio 
+# 532480  web/page.mpfs
 
