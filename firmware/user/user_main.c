36aba34d57874b458724a8d970ba372d9844e8b6
gelakinetic <gelakinetic@gmail.com>
2018-11-14 08:47:10 -0500

    Added SPI_FLASH_SIZE_MAP_OPT3 for good measure
    Maps 2, 3, and 4 all have the same
    SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR, so any of them should work.
    
    Maps 5 and 6 have it elsewhere, which would mess with MFS and saving
    flash settings, so they're definitely invalid

--- firmware/user/user_main.c
+++ firmware/user/user_main.c
@@ -107,6 +107,14 @@
 #define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR_OPT2 0x7c000
 #define SPI_FLASH_SIZE_MAP_OPT2                        2
 
+#define SYSTEM_PARTITION_OTA_SIZE_OPT3                 0x6A000
+#define SYSTEM_PARTITION_OTA_2_ADDR_OPT3               0x81000
+#define SYSTEM_PARTITION_RF_CAL_ADDR_OPT3              0x1fb000
+#define SYSTEM_PARTITION_PHY_DATA_ADDR_OPT3            0x1fc000
+#define SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR_OPT3    0x1fd000
+#define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR_OPT3 0x7c000
+#define SPI_FLASH_SIZE_MAP_OPT3                        3
+
 #define SYSTEM_PARTITION_OTA_SIZE_OPT4                 0x6A000
 #define SYSTEM_PARTITION_OTA_2_ADDR_OPT4               0x81000
 #define SYSTEM_PARTITION_RF_CAL_ADDR_OPT4              0x3fb000
@@ -176,6 +184,15 @@ static const partition_item_t partition_table_opt2[] =
     { SYSTEM_PARTITION_RF_CAL,           SYSTEM_PARTITION_RF_CAL_ADDR_OPT2,           0x1000},
     { SYSTEM_PARTITION_PHY_DATA,         SYSTEM_PARTITION_PHY_DATA_ADDR_OPT2,         0x1000},
     { SYSTEM_PARTITION_SYSTEM_PARAMETER, SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR_OPT2, 0x3000},
+};
+
+static const partition_item_t partition_table_opt3[] =
+{
+    { EAGLE_FLASH_BIN_ADDR,              0x00000,                                     0x10000},
+    { EAGLE_IROM0TEXT_BIN_ADDR,          0x10000,                                     0x60000},
+    { SYSTEM_PARTITION_RF_CAL,           SYSTEM_PARTITION_RF_CAL_ADDR_OPT3,           0x1000},
+    { SYSTEM_PARTITION_PHY_DATA,         SYSTEM_PARTITION_PHY_DATA_ADDR_OPT3,         0x1000},
+    { SYSTEM_PARTITION_SYSTEM_PARAMETER, SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR_OPT3, 0x3000},
 };
 
 static const partition_item_t partition_table_opt4[] =
@@ -813,9 +830,16 @@ void ICACHE_FLASH_ATTR user_pre_init(void)
     {
         os_printf("system_partition_table_regist 4 success!!\r\n");
     }
+    else if(system_partition_table_regist(
+                partition_table_opt3,
+                sizeof(partition_table_opt3) / sizeof(partition_table_opt3[0]),
+                SPI_FLASH_SIZE_MAP_OPT3))
+    {
+        os_printf("system_partition_table_regist 3 success!!\r\n");
+    }
     else
     {
-        os_printf("system_partition_table_regist both fail\r\n");
+        os_printf("system_partition_table_regist all fail\r\n");
         while(1);
     }
 }
