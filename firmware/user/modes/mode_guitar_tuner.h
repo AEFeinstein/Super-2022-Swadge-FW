a3733de47e7202486d0e833718f6557921fb3678
cnlohr <lohr85@gmail.com>
2018-11-30 03:06:40 -0500

    Update guitar tuner code

---
+++ firmware/user/modes/mode_guitar_tuner.h
@@ -0,0 +1,13 @@
+/*
+ * mode_guitar_tuner.h
+ *
+ *  Created on: Nov 30, 2018
+ *      Author: charles
+ */
+
+#ifndef USER_MODE_GUITAR_TUNER_H_
+#define USER_MODE_GUITAR_TUNER_H_
+
+extern swadgeMode guitarTunerMode;
+
+#endif /* USER_MODE_GUITAR_TUNER_H_ */
