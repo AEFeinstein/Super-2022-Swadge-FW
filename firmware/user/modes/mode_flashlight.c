8da75e6304bae5a7e0059dd6fa6e4cdf20d3a95c
gelakinetic <gelakinetic@gmail.com>
2018-12-01 11:19:55 -0500

    Added flashlight strobe

--- firmware/user/modes/mode_flashlight.c
+++ firmware/user/modes/mode_flashlight.c
@@ -21,6 +21,7 @@ void ICACHE_FLASH_ATTR flashlightEnterMode(void);
 void ICACHE_FLASH_ATTR flashlightExitMode(void);
 void ICACHE_FLASH_ATTR flashlightButtonCallback(uint8_t state, int button,
         int down);
+void ICACHE_FLASH_ATTR strobeTimerCallback(void* timer_arg);
 
 /*============================================================================
  * Variables
@@ -47,6 +48,17 @@ static const uint8_t brightnesses[] =
 };
 uint8_t brightnessIdx = 0;
 
+os_timer_t strobeTimer = {0};
+
+static const uint32_t strobePeriodsMs[] =
+{
+    0,
+    500,
+    250,
+};
+uint8_t strobeIdx = 0;
+bool flashlightLedsOn = true;
+
 /*============================================================================
  * Functions
  *==========================================================================*/
@@ -58,9 +70,20 @@ void ICACHE_FLASH_ATTR flashlightEnterMode(void)
 {
     // Turn LEDs on
     brightnessIdx = 0;
+    strobeIdx = 0;
+    flashlightLedsOn = true;
+
     led_t leds[6] = {{0}};
     ets_memset(leds, brightnesses[brightnessIdx], sizeof(leds));
     setLeds(leds, sizeof(leds));
+
+    os_timer_disarm(&strobeTimer);
+    os_timer_setfn(&strobeTimer, strobeTimerCallback, NULL);
+
+    if(0 != strobePeriodsMs[strobeIdx])
+    {
+        os_timer_arm(&strobeTimer, strobePeriodsMs[strobeIdx], true);
+    }
 }
 
 /**
@@ -80,8 +103,8 @@ void ICACHE_FLASH_ATTR flashlightExitMode(void)
  * @param button  The button number which was pressed
  * @param down 1 if the button was pressed, 0 if it was released
  */
-void ICACHE_FLASH_ATTR flashlightButtonCallback(uint8_t state, int button,
-        int down)
+void ICACHE_FLASH_ATTR flashlightButtonCallback(
+    uint8_t state __attribute__((unused)), int button, int down)
 {
     if(down)
     {
@@ -89,11 +112,20 @@ void ICACHE_FLASH_ATTR flashlightButtonCallback(uint8_t state, int button,
         {
             case 1:
             {
+                strobeIdx = (strobeIdx + 1) %
+                            (sizeof(strobePeriodsMs) / sizeof(strobePeriodsMs[0]));
+
+                os_timer_disarm(&strobeTimer);
+                if(0 != strobePeriodsMs[strobeIdx])
+                {
+                    os_timer_arm(&strobeTimer, strobePeriodsMs[strobeIdx], true);
+                }
                 break;
             }
             case 2:
             {
-                brightnessIdx = (brightnessIdx + 1) % (sizeof(brightnesses) / sizeof(brightnesses[0]));
+                brightnessIdx = (brightnessIdx + 1) %
+                                (sizeof(brightnesses) / sizeof(brightnesses[0]));
                 led_t leds[6] = {{0}};
                 ets_memset(leds, brightnesses[brightnessIdx], sizeof(leds));
                 setLeds(leds, sizeof(leds));
@@ -102,4 +134,24 @@ void ICACHE_FLASH_ATTR flashlightButtonCallback(uint8_t state, int button,
             }
         }
     }
+}
+
+/**
+ * Callback used to strobe the flashlight LEDs
+ *
+ * @param timer_arg unused
+ */
+void ICACHE_FLASH_ATTR strobeTimerCallback(
+    void* timer_arg __attribute__((unused)))
+{
+    led_t leds[6] = {{0}};
+    // If LEDs aren't on, turn them on
+    if(!flashlightLedsOn)
+    {
+        ets_memset(leds, brightnesses[brightnessIdx], sizeof(leds));
+    }
+    setLeds(leds, sizeof(leds));
+
+    // Flip
+    flashlightLedsOn = !flashlightLedsOn;
 }
