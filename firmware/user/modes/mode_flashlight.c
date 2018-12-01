1106cd201dd679a82f4c60f2c8f9835f403fd9e7
gelakinetic <gelakinetic@gmail.com>
2018-12-01 11:02:17 -0500

    Added flashlight brightness adjustment

--- firmware/user/modes/mode_flashlight.c
+++ firmware/user/modes/mode_flashlight.c
@@ -19,6 +19,8 @@
 
 void ICACHE_FLASH_ATTR flashlightEnterMode(void);
 void ICACHE_FLASH_ATTR flashlightExitMode(void);
+void ICACHE_FLASH_ATTR flashlightButtonCallback(uint8_t state, int button,
+        int down);
 
 /*============================================================================
  * Variables
@@ -30,12 +32,20 @@ swadgeMode flashlightMode =
     .fnEnterMode = flashlightEnterMode,
     .fnExitMode = flashlightExitMode,
     .fnTimerCallback = NULL,
-    .fnButtonCallback = NULL,
+    .fnButtonCallback = flashlightButtonCallback,
     .fnAudioCallback = NULL,
     .wifiMode = NO_WIFI,
     .fnEspNowRecvCb = NULL,
     .fnEspNowSendCb = NULL,
 };
+
+static const uint8_t brightnesses[] =
+{
+    0x01,
+    0x40,
+    0xFF,
+};
+uint8_t brightnessIdx = 0;
 
 /*============================================================================
  * Functions
@@ -47,8 +57,9 @@ swadgeMode flashlightMode =
 void ICACHE_FLASH_ATTR flashlightEnterMode(void)
 {
     // Turn LEDs on
+    brightnessIdx = 0;
     led_t leds[6] = {{0}};
-    ets_memset(leds, 0xFF, sizeof(leds));
+    ets_memset(leds, brightnesses[brightnessIdx], sizeof(leds));
     setLeds(leds, sizeof(leds));
 }
 
@@ -60,4 +71,35 @@ void ICACHE_FLASH_ATTR flashlightExitMode(void)
     // Turn LEDs off
     led_t leds[6] = {{0x00}};
     setLeds(leds, sizeof(leds));
+}
+
+/**
+ * Adjusts flashlight brightness and strobe
+ *
+ * @param state A bitmask of all button statuses
+ * @param button  The button number which was pressed
+ * @param down 1 if the button was pressed, 0 if it was released
+ */
+void ICACHE_FLASH_ATTR flashlightButtonCallback(uint8_t state, int button,
+        int down)
+{
+    if(down)
+    {
+        switch(button)
+        {
+            case 1:
+            {
+                break;
+            }
+            case 2:
+            {
+                brightnessIdx = (brightnessIdx + 1) % (sizeof(brightnesses) / sizeof(brightnesses[0]));
+                led_t leds[6] = {{0}};
+                ets_memset(leds, brightnesses[brightnessIdx], sizeof(leds));
+                setLeds(leds, sizeof(leds));
+
+                break;
+            }
+        }
+    }
 }
