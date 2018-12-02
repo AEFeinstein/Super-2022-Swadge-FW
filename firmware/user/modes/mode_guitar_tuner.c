fd9f61cf98509543a56185682a3528d56b471e8e
gelakinetic <gelakinetic@gmail.com>
2018-12-02 12:31:45 -0500

    Cleaned up guitar tuner, a lot. Should be no functional changes

--- firmware/user/modes/mode_guitar_tuner.c
+++ firmware/user/modes/mode_guitar_tuner.c
@@ -9,11 +9,25 @@
  * Includes
  *==========================================================================*/
 
+#include <limits.h>
 #include "user_main.h"
 #include "mode_guitar_tuner.h"
 #include "DFT32.h"
 #include "embeddedout.h"
 #include "osapi.h"
+
+/*============================================================================
+ * Defines
+ *==========================================================================*/
+
+#define NUM_STRINGS 6
+#define OFS         0
+#define SENSITIVITY 5 // TODO Change sensitivity here, Adam.
+
+/// Helper macro to return an integer clamped within a range (MIN to MAX)
+#define CLAMP(X, MIN, MAX) ( ((X) > (MAX)) ? (MAX) : ( ((X) < (MIN)) ? (MIN) : (X)) )
+/// Helper macro to return the absolute value of an integer
+#define ABS(X) (((X) < 0) ? -(X) : (X))
 
 /*============================================================================
  * Prototypes
@@ -25,6 +39,8 @@ void ICACHE_FLASH_ATTR guitarTunerSampleHandler(int32_t samp);
 void ICACHE_FLASH_ATTR guitarTunerButtonCallback(uint8_t state __attribute__((unused)),
         int button, int down);
 void ICACHE_FLASH_ATTR guitarLedOverrideReset(void* timer_arg __attribute__((unused)));
+inline int16_t getMagnitude(uint16_t idx);
+inline int16_t getDiffAround(uint16_t idx);
 
 /*============================================================================
  * Variables
@@ -43,10 +59,26 @@ swadgeMode guitarTunerMode =
     .fnEspNowSendCb = NULL,
 };
 
-static int samplesProcessed = 0;
+static int guitarSamplesProcessed = 0;
+static uint32_t intensities_filt[NUM_STRINGS] = {0};
+static int32_t diffs_filt[NUM_STRINGS] = {0};
 
 os_timer_t guitarLedOverrideTimer = {0};
 bool guitarTunerOverrideLeds = false;
+
+/**
+ * Indicies into fuzzed_bins[], a realtime DFT of sorts
+ * fuzzed_bins[0] = A ... 1/2 steps are every 2.
+ */
+const uint16_t freqBinIdxs[NUM_STRINGS] =
+{
+    38, // E string needs to skip an octave... Can't read sounds this low.
+    24, // A string is exactly at note #24
+    34, // D = A + 5 half steps = 34
+    44, // G
+    52, // B
+    62  // e
+};
 
 /*============================================================================
  * Functions
@@ -57,6 +89,7 @@ bool guitarTunerOverrideLeds = false;
  */
 void ICACHE_FLASH_ATTR guitarTunerEnterMode(void)
 {
+    // Piggyback on colorchord's DSP
     InitColorChord();
 
     guitarTunerOverrideLeds = false;
@@ -68,12 +101,35 @@ void ICACHE_FLASH_ATTR guitarTunerEnterMode(void)
 }
 
 /**
- * Called when colorchord is exited, it disarms the timer
+ * Called when guitar tuner is exited, it disarms the timer
  */
 void ICACHE_FLASH_ATTR guitarTunerExitMode(void)
 {
     // Disarm the timer
     os_timer_disarm(&guitarLedOverrideTimer);
+}
+
+/**
+ * Inline helper function to get the magnitude of a frequency bin from fuzzed_bins[]
+ *
+ * @param idx The index to get a magnitude from
+ * @return A signed magnitude, even though fuzzed_bins[] is unsigned
+ */
+inline int16_t getMagnitude(uint16_t idx)
+{
+    return fuzzed_bins[idx];
+}
+
+/**
+ * Inline helper function to get the difference in magnitudes around a given
+ * frequency bin from fuzzed_bins[]
+ *
+ * @param idx The index to get a difference in magnitudes around
+ * @return The difference in magnitudes of the bins before and after the given index
+ */
+inline int16_t getDiffAround(uint16_t idx)
+{
+    return getMagnitude(idx + 1) - getMagnitude(idx - 1);
 }
 
 /**
@@ -87,10 +143,10 @@ void ICACHE_FLASH_ATTR guitarTunerSampleHandler(int32_t samp)
 {
     //os_printf("%s :: %d\r\n", __func__, __LINE__);
     PushSample32( samp );
-    samplesProcessed++;
+    guitarSamplesProcessed++;
 
     // If 128 samples have been processed
-    if( samplesProcessed == 128 )
+    if( guitarSamplesProcessed == 128 )
     {
         // Don't bother if colorchord is inactive
         if( !COLORCHORD_ACTIVE )
@@ -101,199 +157,87 @@ void ICACHE_FLASH_ATTR guitarTunerSampleHandler(int32_t samp)
         // Colorchord magic
         HandleFrameInfo();
 
-#if 0
-        // Update the LEDs as necessary
-        switch( COLORCHORD_OUTPUT_DRIVER )
+        // Guitar tuner magic
+        led_t colors[NUM_STRINGS] = {{0}};
+        uint32_t i;
+        for( i = 0; i < NUM_STRINGS; i++ )
         {
-            case 0:
-            {
-                UpdateLinearLEDs();
-                break;
-            }
-            case 1:
-            {
-                UpdateAllSameLEDs();
-                break;
-            }
-        };
+            // Pick out the current magnitude and filter it
+            intensities_filt[i] = (getMagnitude(freqBinIdxs[i] + OFS)  + intensities_filt[i]) - (intensities_filt[i] >> 5);
 
-        // Push out the LED data
-        if(!guitarOverrideLeds)
-        {
-            setLeds( (led_t*)ledOut, USE_NUM_LIN_LEDS * 3 );
-        }
-#endif
+            // Pick out the difference around current magnitude and filter it too
+            diffs_filt[i] =       (getDiffAround(freqBinIdxs[i] + OFS) + diffs_filt[i])       - (diffs_filt[i] >> 5);
 
-#define OFS 0
-
-        //fuzzed_bins[0] = A ... 1/2 steps are every 2.
-#define ES0 fb[37+OFS]
-#define ES1 fb[39+OFS]
-#define ESI fb[38+OFS]  //E string needs to skip an octave... Can't read sounds this low.
-
-#define AS0 fb[23+OFS]
-#define AS1 fb[25+OFS]
-#define ASI fb[24+OFS]  //A string is exactly at note #24
-
-#define DS0 fb[33+OFS]
-#define DS1 fb[35+OFS]
-#define DSI fb[34+OFS]  //D = A + 5 half steps = 35
-
-#define GS0 fb[43+OFS]
-#define GS1 fb[45+OFS]
-#define GSI fb[44+OFS]
-
-#define BS0 fb[51+OFS]
-#define BS1 fb[53+OFS]
-#define BSI fb[52+OFS]
-
-#define eS0 fb[61+OFS]
-#define eS1 fb[63+OFS]
-#define eSI fb[62+OFS]
-
-        int16_t* fb = (int16_t*)fuzzed_bins;  //Forced typecasting to allow negative results.
-
-        uint16_t intensities_in[6] = {  eSI, BSI, GSI, DSI, ASI, ESI };
-        int16_t  diffs_in[6] =       {  eS1 - eS0, BS1 - BS0, GS1 - GS0, DS1 - DS0, AS1 - AS0, ES1 - ES0 };
-
-        static uint32_t intensities_filt[6];
-        static int32_t diffs_filt[6];
-
-        uint16_t intensities[6];
-        int i;
-        int16_t diffs[6];
-        for( i = 0; i < 6; i++ )
-        {
-            intensities_filt[i] = (intensities_in[i] + intensities_filt[i]) - (intensities_filt[i] >> 5);
-            diffs_filt[i] =       (diffs_in[i] + diffs_filt[i]) - (diffs_filt[i] >> 5);
-
-            //Change sensitivity here, Adam.
-            intensities[i] = intensities_filt[i] >> 5;
-            diffs[i] = diffs_filt[i] >> 5;
-        }
-
-
-        uint8_t colors[18];
-        uint8_t* colorptr = colors;
-        for( i = 0; i < 6; i++ )
-        {
-            int16_t intensity = intensities[i] - 40; // drop a baseline.
-            int16_t  diff = diffs[i];
-
-            int16_t id = intensity;
-            if( intensity < 0 )
-            {
-                intensity = 0;
-            }
-            if( intensity > 255 )
-            {
-                intensity = 255;
-            }
+            // This is the magnitude of the target frequency bin, cleaned up
+            int16_t intensity = (intensities_filt[i] >> SENSITIVITY) - 40; // drop a baseline.
+            intensity = CLAMP(intensity, 0, 255);
 
             //This is the tonal difference.  You "calibrate" out the intensity.
-            int16_t cdiff = diff * 200 / (intensity + 1);
-            int16_t abscdiff = (cdiff > 0) ? cdiff : -cdiff;
+            int16_t tonalDiff = (diffs_filt[i] >> SENSITIVITY) * 200 / (intensity + 1);
 
-            int intune = (abscdiff < 10) ? 1 : 0;
-            int red, grn, blu;
-
-            if( intune )
+            int32_t red, grn, blu;
+            // Is the note in tune, i.e. is the magnitude difference in surrounding bins small?
+            if( (ABS(tonalDiff) < 10) )
             {
+                // Note is in tune, make it white
                 red = 255;
                 grn = 255;
                 blu = 255;
             }
             else
             {
-                if( cdiff > 0 )
+                // Check if the note is sharp or flat
+                if( tonalDiff > 0 )
                 {
+                    // Note too sharp, make it red
                     red = 255;
-                    grn = blu = 255 - (cdiff) * 15;
+                    grn = blu = 255 - (tonalDiff) * 15;
                 }
                 else
                 {
+                    // Note too flat, make it blue
                     blu = 255;
-                    grn = red = 255 - (-cdiff) * 15;
+                    grn = red = 255 - (-tonalDiff) * 15;
                 }
-                if( red > 255 )
-                {
-                    red = 255;
-                }
-                if( blu > 255 )
-                {
-                    blu = 255;
-                }
-                if( grn > 255 )
-                {
-                    grn = 255;
-                }
-                red >> 3;
-                grn >> 3;
-                blu >> 3;
+
+                // Make sure LED output isn't more than 255
+                red = CLAMP(red, INT_MIN, 255);
+                grn = CLAMP(grn, INT_MIN, 255);
+                blu = CLAMP(blu, INT_MIN, 255);
             }
 
+            // Scale each LED's brightness by the filtered intensity for that bin
             red = (red >> 3 ) * ( intensity >> 3);
             grn = (grn >> 3 ) * ( intensity >> 3);
             blu = (blu >> 3 ) * ( intensity >> 3);
 
-            if( red > 255 )
-            {
-                red = 255;
-            }
-            if( blu > 255 )
-            {
-                blu = 255;
-            }
-            if( grn > 255 )
-            {
-                grn = 255;
-            }
-
-            if( red < 0 )
-            {
-                red = 0;
-            }
-            if( blu < 0 )
-            {
-                blu = 0;
-            }
-            if( grn < 0 )
-            {
-                grn = 0;
-            }
-
-            (*(colorptr++)) = grn;
-            (*(colorptr++)) = red;  //red
-            (*(colorptr++)) = blu; //blu
-
-
-            //For hue, 0 is red.
-            //          uint32_t rgb = EHSVtoHEX( , 255, intensity );
-            //          (*(colorptr++)) = rgb;
-            //          (*(colorptr++)) = rgb>>8;
-            //          (*(colorptr++)) = rgb>>16;
+            // Set the LED, ensure each channel is between 0 and 255
+            colors[i].r = CLAMP(red, 0, 255);
+            colors[i].g = CLAMP(grn, 0, 255);
+            colors[i].b = CLAMP(blu, 0, 255);
         }
 
+        // If the LEDs aren't overridden
         if( !guitarTunerOverrideLeds )
         {
-            setLeds( (led_t*)colors, 18 );
+            // Draw the LEDs
+            setLeds( colors, sizeof(colors) );
         }
 
         // Reset the sample count
-        samplesProcessed = 0;
+        guitarSamplesProcessed = 0;
     }
 }
 
 /**
- * Button callback for colorchord. Button 1 adjusts the output LED mode and
- * button 2 adjusts the sensitivity
+ * Button callback for guitar mode. TODO button 2 adjusts the sensitivity
  *
  * @param state  A bitmask of all button states, unused
  * @param button The button which triggered this event
  * @param down   true if the button was pressed, false if it was released
  */
 void ICACHE_FLASH_ATTR guitarTunerButtonCallback(
-    uint8_t state __attribute__((unused)), int button, int down)
+    uint8_t state __attribute__((unused)), int button __attribute__((unused)), int down)
 {
 
     if(down)
@@ -303,41 +247,11 @@ void ICACHE_FLASH_ATTR guitarTunerButtonCallback(
         os_timer_disarm(&guitarLedOverrideTimer);
         os_timer_arm(&guitarLedOverrideTimer, 1000, false);
 #if 0
-
         switch(button)
         {
             case 1:
             {
-                // gCOLORCHORD_OUTPUT_DRIVER can be either 0 for multiple LED
-                // colors or 1 for all the same LED color
-                CCS.gCOLORCHORD_OUTPUT_DRIVER =
-                    (CCS.gCOLORCHORD_OUTPUT_DRIVER + 1) % 2;
-
-                led_t leds[6] = {{0}};
-                if(CCS.gCOLORCHORD_OUTPUT_DRIVER)
-                {
-                    // All the same LED
-                    uint8_t i;
-                    for(i = 0; i < 6; i++)
-                    {
-                        leds[i].r = 0;
-                        leds[i].g = 0;
-                        leds[i].b = 255;
-                    }
-                }
-                else
-                {
-                    // Multiple output colors
-                    uint8_t i;
-                    for(i = 0; i < 6; i++)
-                    {
-                        uint32_t color = getLedColorPerNumber(i, 0xFF);
-                        leds[i].r = (color >>  0) & 0xFF;
-                        leds[i].g = (color >>  8) & 0xFF;
-                        leds[i].b = (color >> 16) & 0xFF;
-                    }
-                }
-                setLeds(leds, sizeof(leds));
+                // Do nothing
                 break;
             }
             case 2:
