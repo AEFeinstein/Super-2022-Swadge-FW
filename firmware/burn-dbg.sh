d2fac3bcedaf8a41bc098f3d0f84e23782053ef4
gelakinetic <gelakinetic@gmail.com>
2019-06-27 20:46:08 -0400

    Updated configs

--- firmware/burn-dbg.sh
+++ firmware/burn-dbg.sh
@@ -2,4 +2,4 @@
 
 /mnt/c/Windows/system32/taskkill.exe /IM "putty.exe" /F
 make burn
-/mnt/c/Windows/system32/cmd.exe /C "START /B putty.exe -serial COM4 -sercfg 74880"
+/mnt/c/Windows/system32/cmd.exe /C "START /B putty.exe -serial COM3 -sercfg 74880"
