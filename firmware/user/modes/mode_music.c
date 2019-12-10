dbd1ab93bfd59676e0b6ebb6c85d74eb7ddcef89
gelakinetic <gelakinetic@gmail.com>
2019-12-09 21:15:41 -0500

    #88 added all note labels for completeness

--- firmware/user/modes/mode_music.c
+++ firmware/user/modes/mode_music.c
@@ -1340,66 +1340,249 @@ char* ICACHE_FLASH_ATTR noteToStr(notePeriod_t note)
     switch(note)
     {
         case SILENCE:
+        {
+            return "SILENCE";
+        }
         case C_0:
+        {
+            return "C0";
+        }
         case C_SHARP_0:
+        {
+            return "C#0";
+        }
         case D_0:
+        {
+            return "D0";
+        }
         case D_SHARP_0:
+        {
+            return "D#0";
+        }
         case E_0:
+        {
+            return "E0";
+        }
         case F_0:
+        {
+            return "F0";
+        }
         case F_SHARP_0:
+        {
+            return "F#0";
+        }
         case G_0:
+        {
+            return "G0";
+        }
         case G_SHARP_0:
+        {
+            return "G#0";
+        }
         case A_0:
+        {
+            return "A0";
+        }
         case A_SHARP_0:
+        {
+            return "A#0";
+        }
         case B_0:
+        {
+            return "B0";
+        }
         case C_1:
+        {
+            return "C1";
+        }
         case C_SHARP_1:
+        {
+            return "C#1";
+        }
         case D_1:
+        {
+            return "D1";
+        }
         case D_SHARP_1:
+        {
+            return "D#1";
+        }
         case E_1:
+        {
+            return "E1";
+        }
         case F_1:
+        {
+            return "F1";
+        }
         case F_SHARP_1:
+        {
+            return "F#1";
+        }
         case G_1:
+        {
+            return "G1";
+        }
         case G_SHARP_1:
+        {
+            return "G#1";
+        }
         case A_1:
+        {
+            return "A1";
+        }
         case A_SHARP_1:
+        {
+            return "A#1";
+        }
         case B_1:
+        {
+            return "B1";
+        }
         case C_2:
+        {
+            return "C2";
+        }
         case C_SHARP_2:
+        {
+            return "C#2";
+        }
         case D_2:
+        {
+            return "D2";
+        }
         case D_SHARP_2:
+        {
+            return "D#2";
+        }
         case E_2:
+        {
+            return "E2";
+        }
         case F_2:
+        {
+            return "F2";
+        }
         case F_SHARP_2:
+        {
+            return "F#2";
+        }
         case G_2:
+        {
+            return "G2";
+        }
         case G_SHARP_2:
+        {
+            return "G#2";
+        }
         case A_2:
+        {
+            return "A2";
+        }
         case A_SHARP_2:
+        {
+            return "A#2";
+        }
         case B_2:
+        {
+            return "B2";
+        }
         case C_3:
+        {
+            return "C3";
+        }
         case C_SHARP_3:
+        {
+            return "C#3";
+        }
         case D_3:
+        {
+            return "D3";
+        }
         case D_SHARP_3:
+        {
+            return "D#3";
+        }
         case E_3:
+        {
+            return "E3";
+        }
         case F_3:
+        {
+            return "F3";
+        }
         case F_SHARP_3:
+        {
+            return "F#3";
+        }
         case G_3:
+        {
+            return "G3";
+        }
         case G_SHARP_3:
+        {
+            return "G#3";
+        }
         case A_3:
+        {
+            return "A3";
+        }
         case A_SHARP_3:
+        {
+            return "A#3";
+        }
         case B_3:
+        {
+            return "B3";
+        }
         case C_4:
+        {
+            return "C4";
+        }
         case C_SHARP_4:
+        {
+            return "C#4";
+        }
         case D_4:
+        {
+            return "D4";
+        }
         case D_SHARP_4:
+        {
+            return "D#4";
+        }
         case E_4:
+        {
+            return "E4";
+        }
         case F_4:
+        {
+            return "F4";
+        }
         case F_SHARP_4:
+        {
+            return "F#4";
+        }
         case G_4:
+        {
+            return "G4";
+        }
         case G_SHARP_4:
+        {
+            return "G#4";
+        }
         case A_4:
+        {
+            return "A4";
+        }
         case A_SHARP_4:
+        {
+            return "A#4";
+        }
         case B_4:
+        {
+            return "B4";
+        }
         case C_5:
         {
             return "C5";
@@ -1501,28 +1684,97 @@ char* ICACHE_FLASH_ATTR noteToStr(notePeriod_t note)
             return "C7";
         }
         case C_SHARP_7:
+        {
+            return "C#7";
+        }
         case D_7:
+        {
+            return "D7";
+        }
         case D_SHARP_7:
+        {
+            return "D#7";
+        }
         case E_7:
+        {
+            return "E7";
+        }
         case F_7:
+        {
+            return "F7";
+        }
         case F_SHARP_7:
+        {
+            return "F#7";
+        }
         case G_7:
+        {
+            return "G7";
+        }
         case G_SHARP_7:
+        {
+            return "G#7";
+        }
         case A_7:
+        {
+            return "A7";
+        }
         case A_SHARP_7:
+        {
+            return "A#7";
+        }
         case B_7:
+        {
+            return "B7";
+        }
         case C_8:
+        {
+            return "C8";
+        }
         case C_SHARP_8:
+        {
+            return "C#8";
+        }
         case D_8:
+        {
+            return "D8";
+        }
         case D_SHARP_8:
+        {
+            return "D#8";
+        }
         case E_8:
+        {
+            return "E8";
+        }
         case F_8:
+        {
+            return "F8";
+        }
         case F_SHARP_8:
+        {
+            return "F#8";
+        }
         case G_8:
+        {
+            return "G8";
+        }
         case G_SHARP_8:
+        {
+            return "G#8";
+        }
         case A_8:
+        {
+            return "A8";
+        }
         case A_SHARP_8:
+        {
+            return "A#8";
+        }
         case B_8:
+        {
+            return "B8";
+        }
         default:
         {
             return "";
