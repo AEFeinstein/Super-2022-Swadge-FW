291908d29e217d72bf4022765f0445daaaa7933b
gelakinetic <gelakinetic@gmail.com>
2019-12-14 18:41:28 -0500

    Updated rtttl2c to read and write to files

--- rtttl2c/rtttl2c.c
+++ rtttl2c/rtttl2c.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 #include "../firmware/user/buzzer.h"
 
 /*============================================================================
@@ -36,7 +37,7 @@ typedef enum
  * Prototypes
  *==========================================================================*/
 
-char* parseNote (char* p, int defscale, int defdur, int bpm);
+char*   parseNote (char* p, int defOctave, int defDuration, int bpm, FILE* outFileC);
 
 /*============================================================================
  * Functions
@@ -51,140 +52,185 @@ char* parseNote (char* p, int defscale, int defdur, int bpm);
  */
 int main(int argc, char** argv)
 {
-    if(2 != argc)
+    if(2 > argc)
     {
         printf("Please pass in an RTTTL string\n");
         return 0;
     }
-    char* ringtone = argv[1];
 
-    char name[16] = {0};
-    int nameIdx = 0;
-    int duration, octave, bpm;
-    int numNotes = 0;
+    FILE* outFileC = fopen("songs.c", "w+");
+    fprintf(outFileC, "#include <songs.h>\n\n");
 
-    rtttlState_t rtttlState = PARSING_NAME;
-    rtttlMetadata_t metadata = DURATION;
+    FILE* outFileH = fopen("songs.h", "w+");
+    fprintf(outFileH, "#include <osapi.h>\n");
+    fprintf(outFileH, "#include \"buzzer.h\"\n");
+    fprintf(outFileH, "#include \"hpatimer.h\"\n");
+    fprintf(outFileH, "#include \"user_main.h\"\n");
 
-    // Iterate through each character, parsing it out one at a time
-    for(char ch = *ringtone; ch != '\0'; ringtone++, ch = *ringtone)
+    char names[50][32] = {{0}};
+
+    int argidx;
+    for(argidx = 1; argidx < argc; argidx++)
     {
-        switch(rtttlState)
+        // Open the file
+        FILE* inFile = fopen(argv[argidx], "r");
+        // Get the length
+        fseek(inFile, 0L, SEEK_END);
+        long sz = ftell(inFile);
+        fseek(inFile, 0L, SEEK_SET);
+        // Read the file
+        char* ringtoneMem = (char*)malloc(sz);
+        fread(ringtoneMem, sz, 1, inFile);
+        // Close the file
+        fclose(inFile);
+
+        char name[32] = {0};
+        int nameIdx = 0;
+        int duration, octave, bpm;
+        int numNotes = 0;
+
+        rtttlState_t rtttlState = PARSING_NAME;
+        rtttlMetadata_t metadata = DURATION;
+
+        // Iterate through each character, parsing it out one at a time
+        char* ringtone = ringtoneMem;
+        for(char ch = *ringtone; ch != '\0'; ringtone++, ch = *ringtone)
         {
-            case PARSING_NAME:
+            switch(rtttlState)
             {
-                if(':' == ch)
+                case PARSING_NAME:
                 {
-                    printf("const song_t %s = {\n", name);
-                    printf("    .notes = {\n");
-                    rtttlState = PARSING_METADATA;
+                    if(':' == ch)
+                    {
+                        fprintf(outFileC, "const song_t %s RODATA_ATTR = {\n", name);
+                        fprintf(outFileC, "    .notes = {\n");
+                        strcat(names[argidx - 1], name);
+                        rtttlState = PARSING_METADATA;
+                    }
+                    else
+                    {
+                        name[nameIdx++] = ch;
+                    }
+                    break;
                 }
-                else
+                case PARSING_METADATA:
                 {
-                    name[nameIdx++] = ch;
-                }
-                break;
-            }
-            case PARSING_METADATA:
-            {
-                switch (ch)
-                {
-                    case ':':
+                    switch (ch)
                     {
-                        rtttlState = PARSING_NOTES;
-                        break;
-                    }
-                    case 'D':
-                    case 'd':
-                    {
-                        metadata = DURATION;
-                        duration = 0;
-                        break;
-                    }
-                    case 'O':
-                    case 'o':
-                    {
-                        metadata = OCTAVE;
-                        octave = 0;
-                        break;
-                    }
-                    case 'B':
-                    case 'b':
-                    {
-                        metadata = BPM;
-                        bpm = 0;
-                        break;
-                    }
-                    case '=':
-                    {
-                        break;
-                    }
-                    case '0':
-                    case '1':
-                    case '2':
-                    case '3':
-                    case '4':
-                    case '5':
-                    case '6':
-                    case '7':
-                    case '8':
-                    case '9':
-                    {
-                        switch (metadata)
+                        case ':':
                         {
-                            case OCTAVE:
-                            {
-                                octave *= 10;
-                                octave += (ch - '0');
-                                break;
-                            }
-                            case DURATION:
-                            {
-                                duration *= 10;
-                                duration += (ch - '0');
-                                break;
-                            }
-                            case BPM:
-                            {
-                                bpm *= 10;
-                                bpm += (ch - '0');
-                                break;
-                            }
+                            rtttlState = PARSING_NOTES;
+                            break;
                         }
-                        break;
+                        case 'D':
+                        case 'd':
+                        {
+                            metadata = DURATION;
+                            duration = 0;
+                            break;
+                        }
+                        case 'O':
+                        case 'o':
+                        {
+                            metadata = OCTAVE;
+                            octave = 0;
+                            break;
+                        }
+                        case 'B':
+                        case 'b':
+                        {
+                            metadata = BPM;
+                            bpm = 0;
+                            break;
+                        }
+                        case '=':
+                        {
+                            break;
+                        }
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                        {
+                            switch (metadata)
+                            {
+                                case OCTAVE:
+                                {
+                                    octave *= 10;
+                                    octave += (ch - '0');
+                                    break;
+                                }
+                                case DURATION:
+                                {
+                                    duration *= 10;
+                                    duration += (ch - '0');
+                                    break;
+                                }
+                                case BPM:
+                                {
+                                    bpm *= 10;
+                                    bpm += (ch - '0');
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                        default:
+                        {
+                            break;
+                        }
                     }
-                    default:
+                    break;
+                }
+                case PARSING_NOTES:
+                {
+                    ringtone = parseNote(ringtone, octave, duration, bpm, outFileC);
+                    if(INTER_NOTE_SILENCE_MS > 0)
                     {
-                        break;
+                        numNotes += 2; // One for the note, one for silence
                     }
+                    else
+                    {
+                        numNotes++;
+                    }
+                    if('\0' == *ringtone)
+                    {
+                        goto ALL_DONE;
+                    }
+                    break;
                 }
-                break;
-            }
-            case PARSING_NOTES:
-            {
-                ringtone = parseNote(ringtone, octave, duration, bpm);
-                if(INTER_NOTE_SILENCE_MS > 0)
-                {
-                    numNotes += 2; // One for the note, one for silence
-                }
-                else
-                {
-                    numNotes++;
-                }
-                if('\0' == *ringtone)
-                {
-                    goto ALL_DONE;
-                }
-                break;
             }
         }
-    }
 
 ALL_DONE:
-    printf("    },\n");
-    printf("    .numNotes = %d,\n", numNotes);
-    printf("    .shouldLoop = false\n");
-    printf("};\n");
+        fprintf(outFileC, "    },\n");
+        fprintf(outFileC, "    .numNotes = %d,\n", numNotes);
+        fprintf(outFileC, "    .shouldLoop = false\n");
+        fprintf(outFileC, "};\n\n");
+        free(ringtoneMem);
+    }
+
+    fprintf(outFileC, "const song_t * allSongs[] = {\n");
+    for(int i = 0; i < argidx - 1; i++)
+    {
+        fprintf(outFileC, "\t&%s,\n", names[i]);
+    }
+    fprintf(outFileC, "};\n\n");
+
+    fprintf(outFileH, "const song_t* ICACHE_FLASH_ATTR getRandSong(void);\n");
+    fprintf(outFileC, "const song_t* ICACHE_FLASH_ATTR getRandSong(void)\n");
+    fprintf(outFileC, "{\n");
+    fprintf(outFileC, "    return allSongs[(os_random() %% (sizeof(allSongs) / sizeof(allSongs[0])))];\n");
+    fprintf(outFileC, "}\n");
+
+    fclose(outFileC);
+    fclose(outFileH);
 }
 
 /**
@@ -196,7 +242,7 @@ ALL_DONE:
  * @param bpm         The beats per minute of the song
  * @return char*      A pointer to the string after the note
  */
-char*   parseNote (char* p, int defOctave, int defDuration, int bpm)
+char*   parseNote (char* p, int defOctave, int defDuration, int bpm, FILE* outFileC)
 {
     int octave = defOctave;
     int octaveMod = 0;
@@ -295,10 +341,10 @@ char*   parseNote (char* p, int defOctave, int defDuration, int bpm)
 
     // Print note
     int noteMs = round(240000 / (float)(bpm * duration)) - INTER_NOTE_SILENCE_MS;
-    printf("        {.note = %s, .timeMs = %d},\n", noteName, noteMs);
+    fprintf(outFileC, "        {.note = %s, .timeMs = %d},\n", noteName, noteMs);
     if(INTER_NOTE_SILENCE_MS > 0)
     {
-        printf("        {.note = %s, .timeMs = %d},\n", SILENCE_STR, INTER_NOTE_SILENCE_MS);
+        fprintf(outFileC, "        {.note = %s, .timeMs = %d},\n", SILENCE_STR, INTER_NOTE_SILENCE_MS);
     }
     return p;
 }
---
+++ rtttl2c/songs.h
@@ -0,0 +1,5 @@
+#include <osapi.h>
+#include "buzzer.h"
+#include "hpatimer.h"
+#include "user_main.h"
+const song_t* ICACHE_FLASH_ATTR getRandSong(void);
