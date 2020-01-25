2cd34f16ef7dbd13346036807c68715145f88df7
gelakinetic <gelakinetic@gmail.com>
2020-01-25 10:36:44 -0500

    Removed unused code

--- firmware/user/ccconfig.h
+++ firmware/user/ccconfig.h
@@ -1,47 +1,6 @@
 #ifndef _CCCONFIG_H
 #define _CCCONFIG_H
 
-#include <c_types.h>
-
-// Set various debug prints all false
-// used when testing with linux, will need modification to work with ESP8266
-#define DFTHIST 0
-#define DEBUGPRINT 0
-#define FUZZHIST 0
-#define SHOWNOTES 0
-#define FOLDHIST 0
-#define SHOWSAMP 0
-#define CHECKOVERFLOW 0
-
-// used by cc with gui to send nice info to o-scope
-#define DFTSAMPLE 0
-
-// progressive DFT is original handling highest octave
-//    with specified sample rate and successive halving
-//    for each subsequent octave
-// if 0 does simple DFT on all bins at given sample rate
-#define PROGRESSIVE_DFT 0
-
-// will change behaviour slight if using progressive DFT
-#define ADJUST_DFT_WITH_OCTAVE 0
-
-//This is for using audio with mic with equal freq response
-//#define USE_EQUALIZER
-
-#define HPABUFFSIZE 512
-
-#define CCEMBEDDED
-#define DEFAULT_NUM_LEDS 16
-#define DFREQ 60
-
-#ifndef DFREQ
-    #define DFREQ 60
-#endif
-
-#ifndef START_INACTIVE
-    #define START_INACTIVE 0
-#endif
-
 #define bzero ets_bzero
 #define delay_us ets_delay_us
 #define get_cpu_frequency ets_get_cpu_frequency
@@ -56,7 +15,7 @@
 #define memmove ets_memmove
 #define memset ets_memset
 #define post ets_post
-// #define printf ets_printf
+#define printf os_printf
 #define putc ets_putc
 #define run ets_run
 #define set_idle_cb ets_set_idle_cb
