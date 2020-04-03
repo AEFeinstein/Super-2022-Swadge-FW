4e0bcdd882b49df485f527f28471cd1cca91ac49
gelakinetic <gelakinetic@gmail.com>
2020-04-02 23:14:48 -0400

    Add --flash-size to the makefile Added comments to PartitionMap.c, commented out unused maps

--- firmware/user/utils/spi_mem/PartitionMap.c
+++ firmware/user/utils/spi_mem/PartitionMap.c
@@ -8,6 +8,7 @@
  * Partition Map Data
  *============================================================================*/
 
+// This is for 1MB (8mbit) SPI flash
 // #define SYSTEM_PARTITION_OTA_SIZE_OPT2                 0x6A000
 // #define SYSTEM_PARTITION_OTA_2_ADDR_OPT2               0x81000
 #define SYSTEM_PARTITION_RF_CAL_ADDR_OPT2              0xfb000
@@ -16,6 +17,7 @@
 // #define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR_OPT2 0x7c000
 #define SPI_FLASH_SIZE_MAP_OPT2                        2
 
+// This is for 2MB (16mbit) SPI flash
 // #define SYSTEM_PARTITION_OTA_SIZE_OPT3                 0x6A000
 // #define SYSTEM_PARTITION_OTA_2_ADDR_OPT3               0x81000
 #define SYSTEM_PARTITION_RF_CAL_ADDR_OPT3              0x1fb000
@@ -24,6 +26,7 @@
 // #define SYSTEM_PARTITION_CUSTOMER_PRIV_PARAM_ADDR_OPT3 0x7c000
 #define SPI_FLASH_SIZE_MAP_OPT3                        3
 
+// This is for 4MB (32mbit) SPI flash
 // #define SYSTEM_PARTITION_OTA_SIZE_OPT4                 0x6A000
 // #define SYSTEM_PARTITION_OTA_2_ADDR_OPT4               0x81000
 #define SYSTEM_PARTITION_RF_CAL_ADDR_OPT4              0x3fb000
@@ -36,6 +39,7 @@
 #define EAGLE_FLASH_BIN_ADDR                 SYSTEM_PARTITION_CUSTOMER_BEGIN + 1
 #define EAGLE_IROM0TEXT_BIN_ADDR             SYSTEM_PARTITION_CUSTOMER_BEGIN + 2
 
+// This is for 1MB (8mbit) SPI flash
 static const partition_item_t partition_table_opt2[] =
 {
     { EAGLE_FLASH_BIN_ADDR,              0x00000,                                     0x10000},
@@ -45,6 +49,7 @@ static const partition_item_t partition_table_opt2[] =
     { SYSTEM_PARTITION_SYSTEM_PARAMETER, SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR_OPT2, 0x3000},
 };
 
+// This is for 2MB (16mbit) SPI flash
 static const partition_item_t partition_table_opt3[] =
 {
     { EAGLE_FLASH_BIN_ADDR,              0x00000,                                     0x10000},
@@ -54,6 +59,7 @@ static const partition_item_t partition_table_opt3[] =
     { SYSTEM_PARTITION_SYSTEM_PARAMETER, SYSTEM_PARTITION_SYSTEM_PARAMETER_ADDR_OPT3, 0x3000},
 };
 
+// This is for 4MB (32mbit) SPI flash
 static const partition_item_t partition_table_opt4[] =
 {
     { EAGLE_FLASH_BIN_ADDR,              0x00000,                                     0x10000},
@@ -67,30 +73,45 @@ static const partition_item_t partition_table_opt4[] =
  * Required function as of ESP8266_NONOS_SDK_v3.0.0. Must call
  * system_partition_table_regist(). This tries to register a few different
  * partition maps. The ESP should be happy with one of them.
+ * 
+ * WARNING
+ * -------
+ * This project uses the ESP-WROOM-02D, which has 16mbit SPI flash.
+ * This corresponds to partition table option 3. How
+ * 
+ * Table 4-2 in the following PDF notes that for a 2MB flash map,
+ * esp_init_data_default_v08.bin gets flashed at 0x1FC000
+ * https://www.espressif.com/sites/default/files/documentation/2a-esp8266-sdk_getting_started_guide_en.pdf
+ * 
+ * This data corresponds to SYSTEM_PARTITION_PHY_DATA_ADDR_OPT3, which is also
+ * 0x1FC000. The data at this address is detailed in the following PDF
+ * https://www.espressif.com/sites/default/files/documentation/esp8266_phy_init_bin_parameter_configuration_guide_en.pdf
+ * 
+ * All of these addresses must match to work. 
  */
 void ICACHE_FLASH_ATTR LoadDefaultPartitionMap(void)
 {
-    if(system_partition_table_regist(
-                partition_table_opt2,
-                sizeof(partition_table_opt2) / sizeof(partition_table_opt2[0]),
-                SPI_FLASH_SIZE_MAP_OPT2))
-    {
-        INIT_PRINTF("system_partition_table_regist 2 success!!\r\n");
-    }
-    else if(system_partition_table_regist(
+    // if(system_partition_table_regist(
+    //             partition_table_opt2,
+    //             sizeof(partition_table_opt2) / sizeof(partition_table_opt2[0]),
+    //             SPI_FLASH_SIZE_MAP_OPT2))
+    // {
+    //     INIT_PRINTF("system_partition_table_regist 2 success!!\r\n");
+    // }
+    /* else */ if(system_partition_table_regist(
                 partition_table_opt3,
                 sizeof(partition_table_opt3) / sizeof(partition_table_opt3[0]),
                 SPI_FLASH_SIZE_MAP_OPT3))
     {
         INIT_PRINTF("system_partition_table_regist 3 success!!\r\n");
     }
-    else if(system_partition_table_regist(
-                partition_table_opt4,
-                sizeof(partition_table_opt4) / sizeof(partition_table_opt4[0]),
-                SPI_FLASH_SIZE_MAP_OPT4))
-    {
-        INIT_PRINTF("system_partition_table_regist 4 success!!\r\n");
-    }
+    // else if(system_partition_table_regist(
+    //             partition_table_opt4,
+    //             sizeof(partition_table_opt4) / sizeof(partition_table_opt4[0]),
+    //             SPI_FLASH_SIZE_MAP_OPT4))
+    // {
+    //     INIT_PRINTF("system_partition_table_regist 4 success!!\r\n");
+    // }
     else
     {
         INIT_PRINTF("system_partition_table_regist all fail\r\n");
